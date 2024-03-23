38fe5f03a1c2240ae4816a28e1baa047b2e7242e
authenticdreamer <tim.weisbarth@student.uni-tuebingen.de>
2024-03-23 16:36:28 +0100

    change max time to 1-20

--- scripts/Exp1/univariate/transformer.sh
+++ scripts/Exp1/univariate/transformer.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name="ftS_transformer_1"
 #SBATCH --gres=gpu:1
 #SBATCH --partition=a100-galvani
-#SBATCH --time 1-12:00:00 #
+#SBATCH --time 1-20:00:00 #
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --output=/mnt/qb/work/ludwig/lqb853/slurm_logs/%x-%j.out  # cannot use $WORK 
