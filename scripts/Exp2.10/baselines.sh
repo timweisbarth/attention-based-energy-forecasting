42ed3df7e6224b5f0b877d670c879f88d6b5c1b9
authenticdreamer <tim.weisbarth@student.uni-tuebingen.de>
2024-04-29 11:04:10 +0200

    .

--- scripts/Exp2.10/baselines.sh
+++ scripts/Exp2.10/baselines.sh
@@ -15,15 +15,12 @@ nvidia-smi # only if you requested any gpus
 #current_folder=$(echo "${0}" | awk -F'/' '{for(i=1; i<=NF; i++) if($i ~ /^Exp/) print $i}')
 current_folder="Exp2.10"
 
-
+# Include DLinear!
 #Needs modification! Check LSTM and TSMixer to be consistent with prior experiments
 for pred_len in 192; do
-    for layers in 2 4; do
-        for dim in 256 512; do
-            for seq_len in 96 336; do
-                for lr in 0.001 0.0005 0.0001; do
+    for hpos in "1 512 96 0.0001" "2 512 96 0.0001" "4 512 96 0.0001" "2 128 96 0.0001" "2 256 96 0.0001" "2 512 96 0.0001" "2 512 96 0.001" "2 512 96 0.0005" "2 512 96 0.0001" "2 512 96 0.0001" "2 512 336 0.0001"
                     #read e_layers d_layers <<< $layer
-                    read d_model n_heads <<< $dim
+                    read layers d_model seq_len lr <<< $hpos
                     srun python3 -u run.py \
                       --is_training 1 \
                       --des $current_folder \
