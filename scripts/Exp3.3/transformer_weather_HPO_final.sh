d7b1fb883366f0bd0eceb9d12a79855517f6ee38
authenticdreamer <tim.weisbarth@student.uni-tuebingen.de>
2024-06-05 10:14:03 +0200

    .

--- scripts/Exp3.3/transformer_weather_HPO_final.sh
+++ scripts/Exp3.3/transformer_weather_HPO_final.sh
@@ -55,6 +55,74 @@ for hpo in "64 6" "128 6" "256 3" "64 3"; do
     done
 done
 
+
+srun python3 -u run.py \
+    --is_training 1 \
+    --des $current_folder \
+    --checkpoints ./checkpoints/$current_folder \
+    --root_path ./data/preproc/ \
+    --data_path smard_plus_weather_without_LUandAT.csv \
+    --model_id 'ftM' \
+    --model Transformer \
+    --data smard_w_weather \
+    --including_weather 1 \
+    --features M \
+    --seq_len 336 \
+    --label_len 168 \
+    --pred_len 192 \
+    --e_layers 6 \
+    --d_layers 6 \
+    --d_model 256 \
+    --d_ff 1024 \
+    --n_heads 8 \
+    --learning_rate 0.0005 \
+    --batch_size 64 \
+    --enc_in 15 \
+    --dec_in 15 \
+    --c_out 15 \
+    --target "load_DE" \
+    --itr 1 \
+    --train_epochs 13 \
+    --patience 6 \
+    --optim adamW \
+    --lradj type1 \
+    --dropout 0.5 \
+    --weight_decay 0.05 \
+
+srun python3 -u run.py \
+    --is_training 1 \
+    --des $current_folder \
+    --checkpoints ./checkpoints/$current_folder \
+    --root_path ./data/preproc/ \
+    --data_path smard_plus_weather_without_LUandAT.csv \
+    --model_id 'ftM' \
+    --model Transformer \
+    --data smard_w_weather \
+    --including_weather 1 \
+    --features M \
+    --seq_len 336 \
+    --label_len 168 \
+    --pred_len 192 \
+    --e_layers 6 \
+    --d_layers 6 \
+    --d_model 256 \
+    --d_ff 1024 \
+    --n_heads 8 \
+    --learning_rate 0.0005 \
+    --batch_size 64 \
+    --enc_in 15 \
+    --dec_in 15 \
+    --c_out 15 \
+    --target "load_DE" \
+    --itr 1 \
+    --train_epochs 13 \
+    --patience 6 \
+    --optim adam \
+    --lradj type1 \
+    --dropout 0.5 \
+    --weight_decay 0.0001 \
+
+
 #DoubleDescent? ca3h
 #srun python3 -u run.py \
 #    --is_training 1 \
